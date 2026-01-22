72a91eaee319f965dc74b431e9235b600978c9e0
eve-124 <sae.miura1224@gmail.com>
2026-01-22 16:02:34 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+
+st.title("血圧・脈拍 平均計算")
+
+max_list = []
+min_list = []
+num_list = []
+
+for i in range(3):
+    st.subheader(f"{i+1}回目")
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        max_list.append(
+            st.number_input(
+                "最高",
+                min_value=50,
+                max_value=250,
+                value=120,
+                step=1,
+                key=f"max{i}"
+            )
+        )
+
+    with col2:
+        min_list.append(
+            st.number_input(
+                "最低",
+                min_value=30,
+                max_value=150,
+                value=80,
+                step=1,
+                key=f"min{i}"
+            )
+        )
+
+    with col3:
+        num_list.append(
+            st.number_input(
+                "脈拍",
+                min_value=30,
+                max_value=200,
+                value=70,
+                step=1,
+                key=f"num{i}"
+            )
+        )
+
+if st.button("平均を計算"):
+    st.header("〔平均〕")
+    st.write(f"最高：{sum(max_list) // 3}")
+    st.write(f"最低：{sum(min_list) // 3}")
+    st.write(f"脈拍：{sum(num_list) // 3}")
+
+st.set_page_config(
+    page_title="血圧計算",
+    layout="centered"
+)
+
